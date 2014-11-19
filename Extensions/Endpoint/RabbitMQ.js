e48f4277ea5120ac22ac9a71c52b46162306d10a
prasannavasan <prasvas23@gmail.com>
2014-11-19 10:01:15 +0530

    RabbitMQ js file inclusion
    Added Skeleton of RabbitMQ Componenet

---
+++ Extensions/Endpoint/RabbitMQ.js
@@ -0,0 +1,40 @@
+var onRecieve = function(){};
+var onError = function(){};
+var onDisconnected = function(){};
+var onConnected = function(){};
+var onInitializing = function(){};
+var onInitialized = function(){};
+
+function start(){
+	
+}
+
+function stop(){
+	
+}
+
+function initialize(parameters){
+
+}
+
+function send(parameters){
+
+}
+
+function trigger(parameters){
+	onRecieve(parameters)
+}
+
+
+exports.endpoint = {
+	start : function(){start()},
+	stop : function(){stop()},
+	initialize: function(parameters){ initialize(parameters)},
+	onError: function(func){ onError = func; },
+	onRecieve: function(func){ onRecieve = func; },
+	onDisconnected: function(func){ onDisconnected = func; },
+	onConnected: function(func){ onConnected = func;},
+	onConnecting: function(func){ onConnecting = func; },
+	trigger: function(parameters){ trigger(parameters) },
+	isStateful: true
+}
